90428b5be8dd13f306b1780a9fdfbe36c8f18a10
Rejoy01 <rejoywilson65@gmail.com>
2024-04-15 22:33:40 +0530

    Codewars challange completed

--- minAndsecondMin.js
+++ minAndsecondMin.js
@@ -34,3 +34,8 @@ function sumTwoSmallestNumbers(numbers) {
     var [ a, b ] = numbers.sort((a, b) => a - b)
     return a + b
   }
+
+  function sumTwoSmallestNumbers(numbers) {  
+    var [ a, b ] = numbers.sort((a, b) => a - b)
+    return a + b
+  }
